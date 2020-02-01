969df13f0f8357b276d8501ecab5f69654d206c6
Jigar-Joisar <40361988+Jigar-Joisar@users.noreply.github.com>
2020-02-01 14:08:19 +0530

    changes

--- getMask.sh
+++ getMask.sh
@@ -1,2 +1,5 @@
 cd yolov3/ 
-python3 detect.py --save-txt --output "../tools/output12" --source "../data/demo" --conf-thres 0.1 --classes 0 1 2
+python3 detect.py --save-txt --output "../tools/output12" --source "../data/demo" --conf-thres 0.1 
+cd ..
+cd tools/
+python3 mask_gen.py
--- tools/mask_gen.py
+++ tools/mask_gen.py
@@ -1,4 +1,23 @@
 import numpy as np    
-mask = np.zeros((227,227))
-mask[70:120,90:150]=1
-import scipy.miscscipy.misc.imsave('mask.jpg', mask)
+import imageio as i
+import os
+import glob
+
+txt_names = glob.glob(os.path.join('./output12/'+'*.txt')) 
+#print(txt_names)
+lines = []
+for t in txt_names:
+  f = open(t)
+  lines = [Line.rstrip() for Line in f]
+  #print(lines)
+
+co = lines[0].split()
+#print(co)
+x1 = int(co[0])
+y1 = int(co[1])
+x2 = int(co[2])
+y2 = int(co[3])
+
+mask = np.zeros((480,640))
+mask[y1:y2,x1:x2]=1
+i.imwrite('mask.jpg', mask)
