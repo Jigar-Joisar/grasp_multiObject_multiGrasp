abc9162af9338d8b1071cbef01a6db60a7c462db
Jigar-Joisar <40361988+Jigar-Joisar@users.noreply.github.com>
2020-02-01 20:11:04 +0530

    Update getMask.sh

--- getMask.sh
+++ getMask.sh
@@ -1,5 +1,5 @@
 cd yolov3/ 
-python3 detect.py --save-txt --output "../tools/output12" --source "../data/demo" --conf-thres 0.1 
+python3 detect.py --save-txt --output "../tools/output" --source "../data/demo_color" --conf-thres 0.3 --classes 41 45 75 40 39
 cd ..
 cd tools/
 python3 mask_gen.py
